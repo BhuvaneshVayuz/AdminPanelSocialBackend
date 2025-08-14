fc91793b150ff8d30f822a8af5d6caaa51bcc39e
bhuvaneshVayuz <bhuvanesh.chaudhary@vayuz.com>
2025-08-14 15:54:09 +0530

    cors

--- app.js
+++ app.js
@@ -16,7 +16,19 @@ import userRoutes from "./routes/userRoutes.js";
 const app = express();
 
 
-app.use(cors());
+const corsOptions = {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+
+
+
+
 app.use(express.json());
 
 
